3dedf9e6eee0964682a4976b87f542fc96f6ad1c
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2023-01-30 21:09:17 +0700

    Create Main.java

---
+++ No3/Main.java
@@ -0,0 +1,14 @@
+//Nama : Taufik Ramdan
+//NIM : 20210040154
+//Class : TI21f
+
+package Nusput.P13.No3;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		TicTacToe tictactoe = new TicTacToe();
+		
+	}
+}
